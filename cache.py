7d567550276cf44c4bc6a150cea25e94d14015bb
Milan08S <miguesanchez82003@gmail.com>
2023-05-03 21:36:49 -0500

    comentarios

--- cache.py
+++ cache.py
@@ -1,2 +1,14 @@
 #Parcial Practico Arquitectura
 #Miguel Angel Sanchez Paez
+
+# numero de vias: 2
+# total de bloques en la cache: 32 bloques
+# numero de sets 16, dos bloques por cada set
+# tamaño offset: 3 bits
+# tamaño index o conjunto: 4 bits
+# tamaño direccionÑ 7 bits
+# total direccion generada por procesador: 14 bits
+# numero de palabras: 8 de 1 byte
+# numero de direcciones de la memoria: 2048
+# funcion de mapeado en asociativa por conjuntos: Set = direccion de bloque(hasta 2048) mod numero de sets en cache (16)
+# tamaño del bloque: 8 bytes  
